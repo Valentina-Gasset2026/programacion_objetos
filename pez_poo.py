5f4d2d9725817880955abe7ce88577d26a5facc5
Valentina-Gasset26 <gassetvale141@gmail.com>
2026-07-18 23:26:36 -0400

    Error corregido Exitosamente, presentando Versión pre-definitiva :D  .

--- pez_poo.py
+++ pez_poo.py
@@ -16,8 +16,8 @@ class Pez :
        # print(f"Hola, este pez es animal acuatico y se llama {self.nombre}, vive en hábitads {self.habitad}, esta especie de pez esta dispersa en {self.lugares_de_origen}")
 
 class Pez_espada(Pez):
-    def __init__(self, nombre, nombre_cientifico, tamaño, peso, habitad, lugares_de_origen, alimento, tipo_de_cuerpo, estado_en_la_cadena_alimenticia,velocidad):
-        super().__init__(nombre, nombre_cientifico, tamaño, peso, habitad, lugares_de_origen, alimento, tipo_de_cuerpo, estado_en_la_cadena_alimenticia,velocidad)
+    def __init__(self, nombre, nombre_cientifico, tamaño, peso, habitad, lugares_de_origen, alimento, tipo_de_cuerpo, estado_en_la_cadena_alimenticia, velocidad):
+        super().__init__(nombre, nombre_cientifico, tamaño, peso, habitad, lugares_de_origen, alimento, tipo_de_cuerpo, estado_en_la_cadena_alimenticia)
         self.velocidad = velocidad
     
     def nadar(self):
@@ -28,14 +28,14 @@ class Pez_espada(Pez):
         print(f"Su nombre cientifico es {self.nombre_cientifico}")
         print(f"Su peso es de {self.peso} kg.")
         print(f"Se tamaño es de {self.tamaño} m.")
-        print(f"viven en hábitads como {self.habitad}")
+        print(f"Viven en hábitads como {self.habitad}")
         print(f"Este pez se encuentra en disperso en {self.lugares_de_origen}")
         print(f"Su velocidad es de {self.velocidad} km/hrs")
         print(f"Este pez se aliemnta de {self.alimento}")
         print(f"Su estado en la cadena alimenticia es {self.estado_en_la_cadena_alimenticia}")
         print(f"Su tipo de cuerpo es {self.tipo_de_cuerpo}")
 
-datos_pez_espada = Pez_espada("Pez Espada", "Xiphias gladius", 4.5, 500, "El Pez Espada vive en aguas tropicales, subtropicales y templadas", "Norte de Hawái, Pacífico Norte y Sur (Perú y Chile), costas orientales de EEUU, México y al este de Japón", "Atún, barracuda, Pez volador, verdel, calamares, etc.", "El Pez Espada es un Pez Oseo", "Este pez se ubica en la cima de la cadena alimenticia, es decir, que es un super depredador, sin embargo, este pez tiene algunos depredadores que lo acechan, dichos depredadores son las Orcas y El marrajo dientuso(un tipo de Tiburón).", 100)
+datos_pez_espada = Pez_espada("Pez Espada", "Xiphias gladius", 4.5, 500, "En aguas tropicales, subtropicales y templadas", "El Norte de Hawái, Pacífico Norte y Sur (Perú y Chile), costas orientales de EEUU, México y al este de Japón", "Atún, barracuda, Pez volador, verdel, calamares, etc.", "Oseo", "En la cima de la cadena alimenticia, es decir, que es un super depredador, sin embargo, este pez tiene algunos depredadores que lo acechan, dichos depredadores son las Orcas y El marrajo dientuso(un tipo de Tiburón).", 100)
 datos_pez_espada.nadar()
 datos_pez_espada.presentar_pez()
 
